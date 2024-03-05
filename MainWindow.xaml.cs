f6933dc07dbee8be7bda75846c4e98fcb4fc22cf
DESKTOP-PEFJP14\zaks <glcasian@outlook.com>
2024-03-05 17:43:21 +0800

    页面布局

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Forms;
+using Microsoft.Win32;
 
 namespace 文件批量重命名
 {
@@ -20,9 +22,24 @@ namespace 文件批量重命名
     /// </summary>
     public partial class MainWindow : Window
     {
+        // 初始值
+        public int InitNum { get; set; }
+        // 递增量
+        public int Increment { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void ChooseDirectoryButton_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.ShowDialog();
+            string d_path = folderBrowserDialog.SelectedPath;
+            this.directoryPathLabel.Content = d_path;
+            Console.WriteLine(d_path);
+        }
+
     }
 }
