f9b040f596f8d8c1f09616139c410aa3c4fd439c
Sebastian-github-2020 <60728148+Sebastian-github-2020@users.noreply.github.com>
2024-11-14 16:16:41 +0800

    增加前后缀处理

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,22 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-using Microsoft.Win32;
-using Path = System.IO.Path;
 
 
 namespace 文件批量重命名
@@ -71,18 +55,25 @@ namespace 文件批量重命名
                     MessageBox.Show("初始值和增量都必填", "温馨提示", MessageBoxButton.OK, MessageBoxImage.Warning);
                 } else
                 {
-                    this.InitNum = int.Parse(this.initNumTextBox.Text);// 初始值
-                    this.Increment = int.Parse(this.incrementTextBox.Text);// 增量
-
-                    DirectoryInfo folder = new DirectoryInfo(this.DirectoryPath);
-                    foreach(FileInfo item in folder.GetFiles())
+                    try
                     {
-                        string newName = item.DirectoryName + "\\" + $"{InitNum}" + $"{item.Extension}";
-                        InitNum += Increment;
+                        this.InitNum = int.Parse(this.initNumTextBox.Text);// 初始值
+                        this.Increment = int.Parse(this.incrementTextBox.Text);// 增量
 
-                        File.Move(item.FullName, newName);
+                        DirectoryInfo folder = new DirectoryInfo(this.DirectoryPath);
+                        foreach(FileInfo item in folder.GetFiles())
+                        {
+                            string newName = item.DirectoryName + "\\" + $"{InitNum}" + $"{item.Extension}";
+                            InitNum += Increment;
+
+                            File.Move(item.FullName, newName);
+                        }
+                        MessageBox.Show("完成", "温馨提示", MessageBoxButton.OK, MessageBoxImage.Information);
+                    } catch(Exception ex)
+                    {
+
+                        MessageBox.Show("只能输入数字", "警告", MessageBoxButton.OK);
                     }
-                    MessageBox.Show("完成", "温馨提示", MessageBoxButton.OK, MessageBoxImage.Information);
 
                 }
             }
@@ -124,5 +115,38 @@ namespace 文件批量重命名
                 }
             }
         }
+        //添加前后缀
+        private void submitButton2_Click(object sender, RoutedEventArgs e)
+        {
+            if(string.IsNullOrEmpty(this.DirectoryPath))
+            {
+                MessageBox.Show("文件夹路径不能为空", "温馨提示", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            } else
+            {
+                // 开始读取目录下的文件
+
+                // 检测 前后缀是否同时为空
+
+                if(string.IsNullOrEmpty(this.prefix.Text) == true && string.IsNullOrEmpty(this.suffix.Text) == true)
+                {
+                    MessageBox.Show("前缀和后缀至少一个", "温馨提示", MessageBoxButton.OK, MessageBoxImage.Warning);
+                } else
+                {
+
+
+                    DirectoryInfo folder = new DirectoryInfo(this.DirectoryPath);
+                    foreach(FileInfo item in folder.GetFiles())
+                    {
+                        string newName = $"{item.DirectoryName}\\{this.prefix.Text ?? ""}{item.Name.Replace(item.Extension, "")}{this.suffix.Text ?? ""}{item.Extension}";
+                        InitNum += Increment;
+                        File.Move(item.FullName, newName);
+                    }
+                    MessageBox.Show("完成", "温馨提示", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                }
+            }
+        }
+
     }
 }
